134dbc9bf33fbd1162ca31265e43288e3497a2c0
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-06-26 22:05:34 +0200

    chore: remove tap-agent crate (#4)
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- tap-agent/src/tap/context/checks.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2023-, GraphOps and Semiotic Labs.
-// SPDX-License-Identifier: Apache-2.0
-
-mod allocation_id;
-mod signature;
-mod value;
-
-pub use allocation_id::AllocationId;
-pub use signature::Signature;
