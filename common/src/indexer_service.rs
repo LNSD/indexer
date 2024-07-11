668c06f7814519362a5a9098f9f81f3b318d641d
Lorenzo Delgado <lorenzo@edgeandnode.com>
2024-07-11 11:40:41 +0200

    refactor(common): flatten indexer service module
    Signed-off-by: Lorenzo Delgado <lorenzo@edgeandnode.com>

--- common/src/indexer_service.rs
+++ common/src/indexer_service.rs
@@ -1,4 +1,19 @@
 // Copyright 2023-, Edge & Node, GraphOps, and Semiotic Labs.
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod http;
+pub use crate::indexer_service::{
+    config::{
+        DatabaseConfig, GraphNetworkConfig, GraphNodeConfig, IndexerConfig, IndexerServiceConfig,
+        ServerConfig, SubgraphConfig, TapConfig,
+    },
+    service::{
+        IndexerService, IndexerServiceImpl, IndexerServiceOptions, IndexerServiceRelease,
+        IndexerServiceResponse,
+    },
+};
+
+mod config;
+mod metrics;
+mod request_handler;
+mod service;
+mod static_subgraph;
